55128387c6893fcc8e6293cd99f50828a058bbd9
NewAvdevs <131870947+NewAvdevs@users.noreply.github.com>
2026-03-25 16:16:02 +0530

    Create quizzes table migration

---
+++ database/migrations/2024_01_01_000004_create_quizzes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateQuizzesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('quizzes', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('lesson_id');
+            $table->unsignedInteger('passing_percentage');
+            $table->timestamps();
+
+            // Foreign key constraint
+            $table->foreign('lesson_id')->references('id')->on('lessons')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('quizzes');
+    }
+}
